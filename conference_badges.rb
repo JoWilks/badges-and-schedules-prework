4600359ac5803ea04c21d8e4bc77eb227f058f2f
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 20:52:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,13 +3,16 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
+  badge_array = []
   name_array.each do |name|
-    badge_maker(name)
+    badge_array<< badge_maker(name)
   end
 end
 
 def assign_rooms(name_array)
+  welcome_array = []
   name_array.each_with_index { |name,index|
   index += 1
-  "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7} 
+  welcome_array<< "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7} 
+  welcome_array
 end
