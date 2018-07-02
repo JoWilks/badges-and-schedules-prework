591795ee9a1a86bf4a7d3a1831614499a73a2da6
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 20:57:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,10 @@ end
 
 def assign_rooms(name_array)
   welcome_array = []
-    name_array.each_with_index { |name,index| index += 1 welcome_array << "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7 } 
+    name_array.each_with_index { |name,index| 
+    index += 1 
+    welcome_array << "Hello, #{name}! You'll be assigned to room #{index}!" 
+    if index <= 7 } 
   welcome_array
 end
 
