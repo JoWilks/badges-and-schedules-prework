388c367a400098320f023d533a04140ba359c543
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 20:42:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def assign_rooms(name_array)
   name_array.each_with_index { |name,index|
   index += 1
   puts "Hello, #{name}! You'll be assigned to room #{index}"
-    
+    if name <
     name_array.length<=7
 end
 
