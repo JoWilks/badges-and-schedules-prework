862a3e784cdeadb4c961c18811c03adb7dbbedba
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 20:59:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,19 +4,14 @@ end
 
 def batch_badge_creator(name_array)
   badge_array = []
-    name_array.each {|name| badge_array<< badge_maker(name)}
+    name_array.each do |name|
+      badge_array<< badge_maker(name)
+    end
   badge_array
 end
 
 def assign_rooms(name_array)
   welcome_array = []
-    name_array.each_with_index do |name,index| if index <= 7
-      index += 1 
-      welcome_array<< "Hello, #{name}! You'll be assigned to room #{index}!"  
-    end
+    name_array.each_with_index { |name,index| index += 1 welcome_array<< "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7} 
   welcome_array
-end
-
-def printer(badges, room_assignments)
-  puts "test"
 end
