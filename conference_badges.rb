78269805ad58219d3e8e8acd464d6a7841454652
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 21:18:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,7 @@ end
 
 def batch_badge_creator(name_array)
   badge_array = []
-    name_array.each do |name|
-      badge_array<< badge_maker(name)
-    end
+    name_array.each { |name| badge_array<< badge_maker(name) }
   badge_array
 end
 
@@ -21,7 +19,11 @@ def assign_rooms(name_array)
 end
 
 def printer(name_array)
-    name_array.each do |name|
-      puts badge_maker(name)
-    end
+    name_array.each { |name|
+      puts badge_maker(name) }
+      
+    name_array.each_with_index { |name,index|
+  index += 1
+  welcome<< "Hello, #{name}! You'll be assigned to room #{index}!" if index <= 7} 
+    
 end
